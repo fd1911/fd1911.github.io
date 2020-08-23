5b4cfdda74419cd9d53633215b625636186558d2
fd1911 <godoyfede@hotmail.com>
2020-08-23 18:56:05 -0300

    file default del wrkspace y creacion de primer desafio

--- clase-1/Files/scripts.js
+++ clase-1/Files/scripts.js
@@ -1,8 +1,31 @@
-// console.log("HelloWorld");
-// alert("ERROR ahre")
-var hello = "Sup mate";
-var numA = 2;
-var numB = 5;
-var saludo = ("Ingrese un saludito:")
+// 1
+console.info("Desafio 1");
 
-alert ("holis")
+var name;
+var greet = "Hello";
+
+name = prompt("Insert your name");
+
+console.info(`${greet} ${name}`);
+
+
+// 2
+console.info("Actividad 2");
+var num1;
+var num2;
+
+num1 = prompt("Ingrese un numero para sumar");
+num2 = prompt("Ingrese segundo numero")
+num1 = parseInt(num1) + parseInt(num2);
+
+console.info(num1);
+
+
+//3
+console.info("Actividad 3")
+text1 = prompt("Ingrese un texto");
+text2 = prompt("Ingrese un texto 2");
+
+alert(`${text1} ${text2}`);
+
+console.warn("Are we done?")
